e41c748b3b69cd11f3c62a6ce82a9ac4c4d3cb94
Dimitri Roche <dimroc@gmail.com>
2013-03-31 18:24:03 -0400

    Add node, grunt, and bower configuration

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          basePath: 'www',
+          sassDir: 'scss',
+          cssDir: 'css'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['compass']);
+};
